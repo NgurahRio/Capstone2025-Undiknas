06c333de720338f12fc1bc42016120204349ba7a
Ngurah Rio <riyoaditya444@gmail.com>
2025-10-28 13:05:07 +0800

    feat(config) : add connection to database

---
+++ backend/config/database.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	_ = godotenv.Load()
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("❌ Database connection error: ", err)
+	}
+
+	fmt.Println("✅ Database connected successfully")
+}
