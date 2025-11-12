a60adf9be18c481688e86414155222be82ef4822
Wahyu Wardaya <147429375+WahyuWardaya@users.noreply.github.com>
2025-11-12 23:40:34 +0800

    add fuction delete user

--- backend/controllers/admin/user/delete_user.go
+++ backend/controllers/admin/user/delete_user.go
@@ -1,1 +1,29 @@
 package user
+
+import (
+	"backend/config"
+	"backend/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// DELETE /admin/users/:id → delete user by ID
+func DeleteUser(c *gin.Context) {
+	id := c.Param("id")
+	var user models.User
+
+	// Check if user exists
+	if err := config.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Pengguna tidak ditemukan"})
+		return
+	}
+
+	// Delete the user
+	if err := config.DB.Delete(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal menghapus pengguna"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Berhasil menghapus pengguna", "data": user})
+}
