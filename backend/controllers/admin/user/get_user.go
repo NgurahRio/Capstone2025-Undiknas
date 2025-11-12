c97f9167a17b9a7ed14af45ab8ffc492428db48e
Ngurah Rio <riyoaditya444@gmail.com>
2025-11-12 23:36:07 +0800

    feat(admin): add controller to get all users and get user by ID

--- backend/controllers/admin/user/get_user.go
+++ backend/controllers/admin/user/get_user.go
@@ -1,1 +1,31 @@
 package user
+
+
+import (
+	"net/http"
+	"backend/config"
+	"backend/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+// GET /admin/users → semua user
+func GetAllUsers(c *gin.Context) {
+	var users []models.User
+	if err := config.DB.Find(&users).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal mengambil data pengguna"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Berhasil mengambil semua pengguna", "data": users})
+}
+
+// GET /admin/users/:id → user by ID
+func GetUserByID(c *gin.Context) {
+	id := c.Param("id")
+	var user models.User
+	if err := config.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Pengguna tidak ditemukan"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Berhasil mengambil data pengguna", "data": user})
+}
