215596edff182e5309aa41296ea1efbd9ce1c8b4
Wahyu Wardaya <147429375+WahyuWardaya@users.noreply.github.com>
2025-11-13 00:44:28 +0800

    implement func get package

--- backend/controllers/admin/group_package/getpackage.go
+++ backend/controllers/admin/group_package/getpackage.go
@@ -1,1 +1,40 @@
 package group_package
+
+import (
+	"backend/config"
+	"backend/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// GET /admin/package → get all packages
+func GetAllPackages(c *gin.Context) {
+	var packages []models.Package
+
+	if err := config.DB.Find(&packages).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal mengambil data package"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Berhasil mengambil semua package",
+		"data":    packages,
+	})
+}
+
+// GET /admin/package/:id → get package by ID
+func GetPackageByID(c *gin.Context) {
+	id := c.Param("id")
+	var pkg models.Package
+
+	if err := config.DB.First(&pkg, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Package tidak ditemukan"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Berhasil mengambil data package",
+		"data":    pkg,
+	})
+}
