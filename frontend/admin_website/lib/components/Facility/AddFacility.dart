6e7ae2ac730caf8fe16afebf08d41330abcaedc6
Denilson <natudenilson@gmail.com>
2025-11-19 19:53:25 +0800

    feat: add facility page UI and functionality and facility creation UI with its functions

---
+++ frontend/admin_website/lib/components/Facility/AddFacility.dart
@@ -0,0 +1,187 @@
+import 'dart:convert';
+import 'package:admin_website/components/ButtonCostum.dart';
+import 'package:admin_website/components/TextFieldCostum.dart';
+import 'package:admin_website/components/WebPickerFile.dart';
+import 'package:admin_website/models/facility_model.dart';
+import 'package:flutter/material.dart';
+import 'dart:typed_data';
+
+class AddFacility extends StatefulWidget {
+  final VoidCallback onClose;
+  final Function(Facility) onSave;
+  final Facility? existingFacility;
+
+  const AddFacility({
+    super.key,
+    required this.onClose,
+    required this.onSave,
+    this.existingFacility,
+  });
+
+
+  @override
+  State<AddFacility> createState() => _AddFacilityState();
+}
+
+class _AddFacilityState extends State<AddFacility> {
+  TextEditingController facilityName = TextEditingController();
+
+  Uint8List? previewIcon;
+  bool isBase64(String data) {
+    return data.length > 100;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.existingFacility != null) {
+      facilityName.text = widget.existingFacility!.name;
+
+      if (isBase64(widget.existingFacility!.icon)) {
+        previewIcon = base64Decode(widget.existingFacility!.icon);
+      } else {
+        previewIcon = null;
+      }
+    }
+  }
+
+  Future<void> onPickImage() async {
+    final result = await pickImageWeb();
+
+    if (result == null) return;
+
+    setState(() {
+      previewIcon = result.bytes;
+    });
+  }
+
+  Widget fieldLabel ({
+    required String text,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, bottom: 5),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 12, 
+          fontWeight: FontWeight.w600
+        ),
+      ),
+    );
+  }
+
+  void clearForm() {
+    facilityName.clear();
+    previewIcon = null;
+  }
+
+  void saveFacility() {
+    final data = Facility(
+      id_facility: widget.existingFacility?.id_facility ??
+          DateTime.now().millisecondsSinceEpoch,
+      name: facilityName.text,
+      icon: previewIcon != null ? base64Encode(previewIcon!) : "",
+    );
+
+    widget.onSave(data);
+    clearForm();
+    widget.onClose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: Card(
+        color: Colors.white,
+        shadowColor: const Color(0x5FA6A6A6),
+        shape: ContinuousRectangleBorder(
+          borderRadius: BorderRadius.circular(10)
+        ),
+        elevation: 10,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.existingFacility == null ? "Create Facility" : "Edit Facility",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w700
+                      ),
+                    ),
+                      
+                    TextButton(
+                      onPressed: widget.onClose, 
+                      child: const Text(
+                        "Close",
+                        style: TextStyle(color: Colors.black),
+                      )
+                    )
+                  ],
+                ),
+              ),
+      
+              fieldLabel(text: "Facility Name"),
+
+              TextFieldCostum(
+                controller: facilityName, 
+                text: "write facility name"
+              ),
+
+              fieldLabel(text: "Icon"),
+
+              Row(
+                children: [
+                  Container(
+                    height: 120,
+                    width: 120,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: previewIcon == null
+                        ? const Center(
+                            child: Text(
+                              "Choose File",
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          )
+                        : Image.memory(
+                            previewIcon!,
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: ButtonCostum3(
+                      icon: Icons.file_upload_outlined, 
+                      text: "upload", 
+                      onTap: onPickImage
+                    ),
+                  )
+                ],
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 25),
+                child: ButtonCostum2(
+                  text: widget.existingFacility == null ? "Save Facility" : "Update Facility",
+                  onPressed: saveFacility
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- frontend/admin_website/lib/pages/content/facility.dart
+++ frontend/admin_website/lib/pages/content/facility.dart
@@ -1,3 +1,12 @@
+import 'package:admin_website/components/ButtonCostum.dart';
+import 'package:admin_website/components/CardCostum.dart';
+import 'package:admin_website/components/DeletePopup.dart';
+import 'package:admin_website/components/Facility/AddFacility.dart';
+import 'package:admin_website/components/HeaderCostum.dart';
+import 'package:admin_website/components/Table/ActionButton.dart';
+import 'package:admin_website/components/Table/TabelContent.dart';
+import 'package:admin_website/components/Table/TableHeader.dart';
+import 'package:admin_website/models/facility_model.dart';
 import 'package:flutter/material.dart';
 
 class FacilityPage extends StatefulWidget {
@@ -8,8 +17,175 @@ class FacilityPage extends StatefulWidget {
 }
 
 class _FacilityPageState extends State<FacilityPage> {
+  TextEditingController searchFacility = TextEditingController();
+
+  bool openAddFasility = false;
+
+  Facility? editingFacility;
+  List<Facility> facilitySearch = [];
+
+  void _searchFunction() {
+    String query = searchFacility.text.toLowerCase();
+
+    setState(() {
+      if (query.isEmpty) {
+        facilitySearch = facilities;
+      } else {
+        facilitySearch = facilities.where(
+          (dest) => dest.name.toLowerCase().contains(query)
+        ).toList();
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    facilitySearch = facilities;
+    searchFacility.addListener(_searchFunction);
+  }
+
+  void deleteFacility(int id) {
+    showPopUpDelete(
+      context: context, 
+      text: "Facility", 
+      onDelete: () {
+        setState(() {
+          facilities.removeWhere((item) => item.id_facility == id);
+
+          searchFacility.clear();
+        });
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFFF3F9FF),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                
+                HeaderCostum(controller: searchFacility),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 35),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Manage Facilities",
+                        style: TextStyle(fontSize: 20),
+                      ),
+
+                      ButtonCostum(
+                        text: "Add Facility", 
+                        onPressed: () {
+                          setState(() {
+                            openAddFasility = true;
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                ),
+
+                if(openAddFasility)
+                  AddFacility(
+                    existingFacility: editingFacility,
+                    onClose: () {
+                      setState(() {
+                        openAddFasility = false;
+                        editingFacility = null;
+                      });
+                    },
+                    onSave: (updatedFacility) {
+                      setState(() {
+                        if (editingFacility == null) {
+                          facilities.add(updatedFacility);
+                        } else {
+                          final index = facilities.indexWhere(
+                              (f) => f.id_facility == updatedFacility.id_facility);
+                          facilities[index] = updatedFacility;
+                        }
+                      });
+                    },
+                  ),
+
+                CardCostum(
+                  content: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          children: [
+                            TableHeader(title: "Icon"),
+                            TableHeader(title: "Name", flex: 2,),
+                            TableHeader(title: "Actions", flex: 5,),             
+                          ],
+                        ),
+                      ),
+
+                      const Divider(height: 1,),
+
+                      ...facilitySearch.asMap().entries.map((entry) {
+                        final index = entry.key;
+                        final fac = entry.value;
+
+                        final bool isEven = index % 2 == 0;
+
+                        return Container(
+                          color: isEven ? Colors.white : const Color.fromARGB(255, 237, 246, 255), // warna berbeda
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Row(
+                              children: [
+                                TableContent(
+                                  isIcon: true, 
+                                  title: fac.icon,
+                                ),
+                                TableContent(title: fac.name, flex: 2,),
+
+                                Expanded(
+                                  flex: 5,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Actionbutton(
+                                        label: "Edit",
+                                        onTap: () {
+                                          setState(() {
+                                            openAddFasility = true;
+                                            editingFacility =  fac;
+                                          });
+                                        },
+                                      ),
+                                      Actionbutton(
+                                        isDelete: true,
+                                        label: "Delete", 
+                                        onTap: () => deleteFacility(fac.id_facility), 
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ],
+                  )
+                )
+              ],
+            ),
+          ),
+        )
+      ),
+    );
   }
 }
