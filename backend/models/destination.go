349c19cf6f42fc05ab477b1c8f0b24108edcfe13
Ngurah Rio <riyoaditya444@gmail.com>
2025-12-16 00:37:02 +0800

    feat(models): add operational field to destination model

--- backend/models/destination.go
+++ backend/models/destination.go
@@ -17,6 +17,7 @@ type Destination struct {
 	FacilityID    string  `gorm:"column:facilityId;type:longtext" json:"facilityId"`
 	Longitude     float64 `gorm:"column:longitude" json:"longitude"`
 	Latitude      float64 `gorm:"column:latitude" json:"latitude"`
+	Operational   string  `gorm:"column:operational" json:"operational"`
 
 	Sos           SOS           `gorm:"foreignKey:SosID;references:ID" json:"sos,omitempty"`
 	Subcategories []Subcategory `gorm:"many2many:destination_subcategories;joinForeignKey:ID;joinReferences:ID" json:"subcategories,omitempty"`
