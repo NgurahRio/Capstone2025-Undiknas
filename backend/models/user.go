01f39add87fd89940bf8d43f93f8e9c77db588cd
Ngurah Rio <riyoaditya444@gmail.com>
2025-12-15 16:37:14 +0800

    fix(models): add soft delete support to User model

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,12 +1,15 @@
 package models
 
+import "gorm.io/gorm"
+
 type User struct {
-	ID       uint   `gorm:"primaryKey;column:id_users" json:"id_users"`
-	Username string `gorm:"column:username;not null;unique" json:"username"`
-	Password string `gorm:"column:password;not null" json:"password"`
-	RoleID   uint   `gorm:"column:roleId" json:"roleId"`
-	Email    string `gorm:"column:email;not null" json:"email"`
-	Image    []byte `gorm:"column:image;" json:"image"`
+	ID        uint           `gorm:"primaryKey;column:id_users" json:"id_users"`
+	Username  string         `gorm:"column:username;not null;unique" json:"username"`
+	Password  string         `gorm:"column:password;not null" json:"password"`
+	RoleID    uint           `gorm:"column:roleId" json:"roleId"`
+	Email     string         `gorm:"column:email;not null" json:"email"`
+	Image     []byte         `gorm:"column:image;" json:"image"`
+	DeletedAt gorm.DeletedAt `gorm:"column:delete_at;index" json:"delete_at"`
 }
 
 func (User) TableName() string {
