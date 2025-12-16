ffa4fcdaa1b36398bb5ae32280d87ea3a04c70ff
Ngurah Rio <riyoaditya444@gmail.com>
2025-12-17 00:54:28 +0800

    feat(models): add Destination relation to Packages model

--- backend/models/packages.go
+++ backend/models/packages.go
@@ -4,6 +4,8 @@ type Packages struct {
 	ID             uint   `gorm:"primaryKey;column:id_packages" json:"id_packages"`
 	DestinationID  uint   `gorm:"column:destinationId" json:"destinationId"`
 	SubPackageData string `gorm:"column:subpackage_data" json:"subpackage_data"`
+
+	Destination Destination `gorm:"foreignKey:DestinationID;references:ID" json:"destination,omitempty"`
 }
 
 func (Packages) TableName() string {
