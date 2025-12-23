8782854cca1b2e2cb1c85a5bc6718ad640e7464b
Wahyu Wardaya <147429375+WahyuWardaya@users.noreply.github.com>
2025-12-23 17:12:57 +0800

    feat(favorite): change DestinationID and EventID to pointer types for optional values

--- backend/models/favorite.go
+++ backend/models/favorite.go
@@ -3,8 +3,8 @@ package models
 type Favorite struct {
 	ID            uint `gorm:"primaryKey;column:id_bookmark" json:"id_bookmark"`
 	UserID        uint `gorm:"column:userId" json:"userId"`
-	DestinationID uint `gorm:"column:destinationId" json:"destinationId"`
-	EventID       uint `gorm:"column:eventId" json:"eventId"`
+	DestinationID *uint `gorm:"column:destinationId" json:"destinationId"`
+	EventID       *uint `gorm:"column:eventId" json:"eventId,omitempty"`
 }
 
 func (Favorite) TableName() string {
